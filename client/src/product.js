eb3e3f49d5b4872a52493df78dc327e528fa9cea
Ahmed Al Qady <26540442+ABQady@users.noreply.github.com>
2023-02-12 15:38:36 +0200

    Added conditional rendering

--- client/src/product.js
+++ client/src/product.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Component } from 'react';
 
 class Product extends Component {
+
    render() {
-      const { SKU, Name, Price, Size, Weight, Dimensions } = this.props;
+      const { SKU, Name, Price, Size, Weight, Dimensions, Type } = this.props;
+
+      let fields;
+      if (this.props.Type == 'DVD') {
+         fields = <h5 id='size' >{Size} MB</h5>;
+      } else if (this.props.Type == 'Book') {
+         fields = <h5 id='weight'>{Weight} KG</h5>
+      } else if (this.props.Type == 'Furniture') {
+         fields = <h5 id='dimensions'>{Dimensions} HxWxL</h5>
+      }
+
       return (
          <div className='border p-3 m-3 col-10 col-sm-6 col-md-4 col-lg-3'>
             <input type="checkbox" className='ms-3 form-check-input delete-checkbox' value={SKU}></input>
@@ -11,9 +22,7 @@ class Product extends Component {
                <h5>{SKU}</h5>
                <h5>{Name}</h5>
                <h5>{Price} $</h5>
-               <h5>{Size} MB</h5>
-               <h5>{Weight} KG</h5>
-               <h5>{Dimensions} HxWxL</h5>
+               {fields}
             </div>
          </div>
       )
